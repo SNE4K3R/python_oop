e959e175c5a9a267b481fb2ff9d852321d7cc5cd
oliveralka <randalthor89@gmx.net>
2020-10-12 17:56:35 +0200

    ADD classes; fasta

---
+++ DecoyGenerator.py
@@ -0,0 +1,77 @@
+# fasta -> read() -> class xxx -> DecoyGenerator -> prot/rna seq -> decoysequene -> write() 
+
+# class FastaFile    
+    #identifier # label 
+    #Sequence
+    #DecoySequence
+    #read()  # biopython
+    #write() # biopython
+
+#class DecoyGenerator
+    #method reverse_protein_seq() - check internally if protein or RNA
+    #method reverse_rna_seq()
+    
+#class Sequence 
+   #method checkValid
+
+# class Protein_seq
+    #method checkValid
+        
+# RNA Sequence (modifications in the code -> can not do a reverse string)
+   #method checkValid
+    # prases everything within bracets as asingle unit
+
+# package reading fasta files (biopython)
+import os.path
+from Bio import SeqIO
+ 
+class Entry:
+    # parameterized constructor - additional add default value if nothing is passed
+    def __init__(self, 
+                 identifier="",
+                 sequence="",
+                 decoy_sequence=""):
+        self.identifier = identifier
+        self.sequence = sequence
+        self.decoy_sequence = decoy_sequence
+        
+class DecoyGenerator:
+    def reverseSequence():
+         # check which sequence? 
+        pass
+
+class FastaFile:
+   
+    @staticmethod
+    def read(path):
+        entries = [] # TODO:would you do it this way? 
+        extension = os.path.splitext(path)[1].strip('.')
+        for seq_record in SeqIO.parse(path, extension):
+            entry = Entry(identifier=seq_record.id)
+            entry.identifier = seq_record.id
+            entries.append(entry)
+        return entries
+    
+    @staticmethod
+    def write(entries, path):
+        pass
+    
+class Sequence: # base class
+    def checkValid():
+        pass
+
+class ProteinSequence(Sequence): # inheritance
+    def checkValid(): # overload
+        pass
+
+class RNASequence(Sequence): # inheritance
+    def checkValid(): # overload
+        pass
+
+if __name__ == "__main__": 
+    
+    path = "/Users/alka/Documents/work/teaching/20SS/VK/python_oop/protein_small_test.fasta"
+    entries = FastaFile.read(path)
+        
+    for elements in entries:
+        print(elements.identifier)
