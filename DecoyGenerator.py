4caa2cc1d1858fb5429f59169d42dfa01e51edc3
oliveralka <pancake@DESKTOP-D91JH14.localdomain>
2020-10-12 19:47:18 +0200

    ADD Seq

--- DecoyGenerator.py
+++ DecoyGenerator.py
@@ -25,14 +25,34 @@
 import os.path
 from Bio import SeqIO
  
+
+class Sequence: # base class
+    def __init__(self,
+                 sequence=""):
+        self.sequence = sequence
+
+    def __str__(self): # gives back string instead of class attribute
+        return "%s" %(self.sequence)
+    
+    def checkValid():
+        pass
+
+class ProteinSequence(Sequence): # inheritance
+    def checkValid(): # overload
+        pass
+
+class RNASequence(Sequence): # inheritance
+    def checkValid(): # overload
+        pass
+
 class Entry:
     # parameterized constructor - additional add default value if nothing is passed
     def __init__(self, 
                  identifier="",
-                 sequence="",
+                 sequence=Sequence(),
                  decoy_sequence=""):
         self.identifier = identifier
-        self.sequence = sequence
+        self.sequence = Sequence(sequence)
         self.decoy_sequence = decoy_sequence
         
 class DecoyGenerator:
@@ -49,29 +69,21 @@ class FastaFile:
         for seq_record in SeqIO.parse(path, extension):
             entry = Entry(identifier=seq_record.id)
             entry.identifier = seq_record.id
+            entry.sequence = Sequence(seq_record.seq)
             entries.append(entry)
         return entries
     
     @staticmethod
     def write(entries, path):
         pass
-    
-class Sequence: # base class
-    def checkValid():
-        pass
-
-class ProteinSequence(Sequence): # inheritance
-    def checkValid(): # overload
-        pass
-
-class RNASequence(Sequence): # inheritance
-    def checkValid(): # overload
-        pass
 
 if __name__ == "__main__": 
     
-    path = "/Users/alka/Documents/work/teaching/20SS/VK/python_oop/protein_small_test.fasta"
+    #path = "/Users/alka/Documents/work/teaching/20SS/VK/python_oop/protein_small_test.fasta"
+    path = "/mnt/e/debian_prog/vorkurs_python_oop/protein_small_test.fasta"
     entries = FastaFile.read(path)
         
     for elements in entries:
         print(elements.identifier)
+        print(type(elements.sequence))
+        print(elements.sequence)
