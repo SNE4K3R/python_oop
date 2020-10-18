f7ffd3aa0fbb3d0fe6be25a785d17dbf21b48bbb
oliveralka <randalthor89@gmx.net>
2020-10-18 12:43:24 +0200

    [ADD] writer, minor changes

--- DecoyGenerator.py
+++ DecoyGenerator.py
@@ -78,52 +78,82 @@ class RNASequence(Sequence): #inheritance
 class Entry:
     # parameterized constructor - additional add default value if nothing is passed
     def __init__(self, 
-                 identifier="",
+                 description="",
+                 decoy_description="",
                  sequence="",
                  decoy_sequence=""):
-        self.identifier = identifier #does not need to be encapsulated since it is always valid
+        self.description = description #does not need to be encapsulated since it is always valid
+        self.decoy_description = decoy_description
         self.sequence = Sequence(sequence)
         self.decoy_sequence = Sequence(decoy_sequence)
         
 # works with all subclasses of Sequence
+# TODO: not optimal, since it sets the DECOY_ in front of the identifier
 class DecoyGenerator:
+    @staticmethod
+    def generateDecoyID(description):
+        decoy_description = "Decoy_" + description
+        return decoy_description
+
     @staticmethod
     def reverseSequence(seq: Sequence):
         rev_seq = Sequence.reverseSequence(seq)
         return rev_seq
 
+    @staticmethod
+    def generateDecoy(entry: Entry):
+        entry.decoy_description = DecoyGenerator.generateDecoyID(entry.description)
+        entry.decoy_sequence = DecoyGenerator.reverseSequence(entry.sequence)
+
 # TODO: Difference between Protein / RNA sequence not general,
 # since this is checked internally.
 class FastaFile:
+
+    # the fasta format standarization has a newline every 60 characters
+    # for better readability.
+    @staticmethod
+    def insert_newlines(string, every=60):
+        lines = []
+        for i in range(0, len(string), every):
+            lines.append(string[i:i+every])
+        return '\n'.join(lines)
+
     @staticmethod
     def read(path):
         entries = []
         extension = os.path.splitext(path)[1].strip('.')
         for seq_record in SeqIO.parse(path, extension):
-            entry = Entry(identifier=seq_record.id)
-            entry.identifier = seq_record.id
-            if entry.identifier.startswith("sp"): #swissprot specific
+            entry = Entry()
+            entry.description = seq_record.description
+            if entry.description.startswith("sp"): #swissprot specific
                 entry.sequence = ProteinSequence(seq_record.seq)
-            if not entry.identifier.startswith("sp") and "RNA" in entry.identifier:
+            if not entry.description.startswith("sp") and "RNA" in entry.description:
                 entry.sequence = RNASequence(seq_record.seq)
             entries.append(entry)
         return entries
     
     @staticmethod
     def write(entries, path):
-        pass
+        f = open(path, "w")
+        for elements in entries:
+            f.writelines(">" + elements.description + "\n")
+            writeable_seq = FastaFile.insert_newlines(elements.sequence.getAsString())
+            f.writelines(writeable_seq + "\n")
+            f.writelines(">" + elements.decoy_description + "\n")
+            writeable_decoy_seq = FastaFile.insert_newlines(elements.decoy_sequence.getAsString())
+            f.writelines(writeable_decoy_seq + "\n")
+        f.close()
+
 
 if __name__ == "__main__": 
     
-    #path = "/Users/alka/Documents/work/teaching/20SS/VK/python_oop/protein_small_test.fasta"
-    #path = "/mnt/e/debian_prog/vorkurs_python_oop/protein_small_test.fasta"
-    path = "/Users/alka/Documents/work/teaching/20SS/VK/python_oop/protein_rna.fasta"
-    entries = FastaFile.read(path)
-        
-    for elements in entries:
-        elements.decoy_sequence = DecoyGenerator.reverseSequence(elements.sequence)
+    #path_in = "/Users/alka/Documents/work/teaching/20SS/VK/python_oop/protein_small_test.fasta"
+    #path_in = "/mnt/e/debian_prog/vorkurs_python_oop/protein_small_test.fasta"
+    path_in = "/Users/alka/Documents/work/teaching/20SS/VK/python_oop/protein_rna.fasta"
+    path_out = "/Users/alka/Documents/work/teaching/20SS/VK/python_oop/decoydatabase.fasta"
+    entries = FastaFile.read(path_in)
 
-        print(elements.identifier)
-        print(type(elements.sequence))
-        #print(elements.sequence)
-        #print(elements.decoy_sequence)
+    for elements in entries:
+        elements = DecoyGenerator.generateDecoy(elements)
+
+    FastaFile.write(entries, path_out)
